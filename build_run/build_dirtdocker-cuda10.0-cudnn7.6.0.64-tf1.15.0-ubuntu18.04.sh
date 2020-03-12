293703bd3bb8fac0f0ad873d703726b52cd3595a
Anurag Uppuluri <auppulur@f22.csc.calpoly.edu>
2020-03-11 17:11:45 -0700

    egl not matching active cuda even after using built-in nvidia gpu support

--- build_run/build_dirtdocker-cuda10.0-cudnn7.6.0.64-tf1.15.0-ubuntu18.04.sh
+++ build_run/build_dirtdocker-cuda10.0-cudnn7.6.0.64-tf1.15.0-ubuntu18.04.sh
@@ -3,9 +3,4 @@ export CUDA_BASE_VERSION=10.0
 export CUDNN_VERSION=7.6.0.64
 export UBUNTU_VERSION=18.04
 export TENSORFLOW_VERSION=1.15.0
-docker build -t dirtdocker/latest:cuda10.0-cudnn7.6.0.64-tf1.15.0-ubuntu18.04 \
-    --build-arg CUDA_BASE_VERSION=$(echo $CUDA_BASE_VERSION) \
-    --build-arg TENSORFLOW_VERSION=$(echo $TENSORFLOW_VERSION) \
-    --build-arg CUDNN_VERSION=$(echo $CUDNN_VERSION) \
-    --build-arg UBUNTU_VERSION=$(echo $UBUNTU_VERSION) . | \
-    tee dirt-build-log-ca10.0-cn7.6.0.64-tf1.15.0-ub18.04-cout-no-sq-test.txt
+docker build -t dirtdocker/latest:cuda10.0-cudnn7.6.0.64-tf1.15.0-ubuntu18.04 --build-arg CUDA_BASE_VERSION=$(echo $CUDA_BASE_VERSION) --build-arg TENSORFLOW_VERSION=$(echo $TENSORFLOW_VERSION) --build-arg CUDNN_VERSION=$(echo $CUDNN_VERSION) --build-arg UBUNTU_VERSION=$(echo $UBUNTU_VERSION) . | tee dirt-build-log-ca10.0-cn7.6.0.64-tf1.15.0-ub18.04-cout-no-sq-test.txt
--- build_run/start_dirtdocker-cuda10.0-cudnn7.6.0.64-tf1.15.0-ubuntu18.04.sh
+++ build_run/start_dirtdocker-cuda10.0-cudnn7.6.0.64-tf1.15.0-ubuntu18.04.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 # usage: ./start_dirtdocker-cuda10.0-cudnn7.6.0.64-tf1.15.0-ubuntu18.04.sh <GPU number>
-docker run -u $(id -u):$(id -g) --rm --runtime=nvidia -it -v /data:/data -e CUDA_VISIBLE_DEVICES=$1 -e USER=$USER -e HOME=/data/$USER -w $PWD dirtdocker/latest:cuda10.0-cudnn7.6.0.64-tf1.15.0-ubuntu18.04 bash
+#docker run -u $(id -u):$(id -g) --rm --runtime=nvidia -it -v /data:/data -e CUDA_VISIBLE_DEVICES=$1 -e USER=$USER -e HOME=/data/$USER -w $PWD dirtdocker/latest:cuda10.0-cudnn7.6.0.64-tf1.15.0-ubuntu18.04 bash
+# new usage: <bash ./start_dirtdocker-cuda10.0-cudnn7.6.0.64-tf1.15.0-ubuntu18.04.sh> can change GPU number in docker run cmd below 
+docker run --gpus '"device=0"' -u $(id -u):$(id -g) --rm -it -v /data:/data -e USER=$USER -e HOME=/data/$USER -w $PWD dirtdocker/latest:cuda10.0-cudnn7.6.0.64-tf1.15.0-ubuntu18.04 bash
 
